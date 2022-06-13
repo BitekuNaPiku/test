dbf29d9e7c97a8f76ddb1fb5afdabd80d9d5e918
BitekuNaPiku <106530299+BitekuNaPiku@users.noreply.github.com>
2022-06-13 13:48:34 +0200

    das
    das

--- fv.py
+++ fv.py
@@ -1,4 +1,15 @@
-print("Ahoj")
-a = int(input("Jak velký je čtverec?"))
-b = (a*a)
-print('Obsah čtverce je =', b)
+#print/proměné
+ strana = float(input('Zadej stranu čtverce v centimetrech: '))
+ print('Obvod čtverce se stranou', strana, 'je', 4 * strana, 'cm')
+ print('Obsah čtverce se stranou', strana, 'je', strana * strana, 'cm2')
+
+#range/mocniny
+ for k in range(1, 10):
+     print(k*k, end=" ")
+
+#if
+ cislo = int(input("Zadej číslo: "))
+ if cislo > 0:
+    print("Zadané číslo je kladné.", cislo)
+ else:
+    print("Zadané číslo není kladné.", cislo)
