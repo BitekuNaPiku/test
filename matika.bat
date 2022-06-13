9e2ec66a4f0857da6e49fea7e81a4d6291ec290d
Felda77 <106530333+Felda77@users.noreply.github.com>
2022-06-13 12:56:12 +0200

    Update matika.bat

--- matika.bat
+++ matika.bat
@@ -2,7 +2,15 @@ echo off
 title Pocitame prumer
 set /p prvni=zadej kolik jablek ma Honza : 
 set /p druhy=zadej kolik jablek ma Jarda : 
-set /a soucet=%prvni%+%druhy%
-set /a prumer=%soucet%/2
+set /p treti=zadej kolik jablek ma Pepa :
+set /p ctvrty=zadej kolik jablek ma Jarda:
+set /p paty=zadej kolik jablek ma Jarmila:
+set /a soucet=%prvni%+%druhy%+%treti%+%ctvrty%+%paty%
+set /a prumer=%soucet%/5
+set /a soucet=%soucet%
+set /a rozdil=%prvni%-%druhy%-%treti%-%ctvrty%-%paty%
+set /a rozdil=%rozdil%
+echo Rozdil jablek je %rozdil%
+echo Soucet jablek je %soucet%
  echo  Prumer jablek je %prumer%
  pause
