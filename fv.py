169c5fe40dd63a1ba527305393d948a27e7b93ee
BitekuNaPiku <106530299+BitekuNaPiku@users.noreply.github.com>
2022-06-20 13:34:34 +0200

    password
    s

--- fv.py
+++
@@ -1,9 +0,0 @@
-print("Ahoj")
-strana = float(input('Zadej stranu čtverce v centimetrech: '))
- print("Obvod čtverce se stranou", strana, "je", 4 * strana, "cm")
- print("Obsah čtverce se stranou", strana, "je", strana * strana, "cm2")
- for k in range(1, 10):
-     print(k*k, end=" ")
- cislo = 2
- if cislo > 0: print("Zadané číslo je kladné.", cislo)
- else: print("Zadané číslo není kladné.", cislo)
